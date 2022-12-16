8595a3ef6e15ef67461d024e9fcb5bdf897262c6
bill <Lin_k_Bill@163.com>
2022-12-16 22:14:15 +0800

    add: less-loader

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
  * @Author: bill Lin_k_Bill@163.com
  * @Date: 2022-12-16 15:56:43
  * @LastEditors: bill Lin_k_Bill@163.com
- * @LastEditTime: 2022-12-16 16:09:39
+ * @LastEditTime: 2022-12-16 22:13:20
  * @FilePath: /webpack-demo01/src/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import './index.less'
 class Test {
   constructor() {
     document.write('hello world')
