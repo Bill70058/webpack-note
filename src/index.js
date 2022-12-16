2ba1c68f27c0aa21b2d1b9878ee76a4b3a23f35c
bill <Lin_k_Bill@163.com>
2022-12-16 22:04:05 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: bill Lin_k_Bill@163.com
+ * @Date: 2022-12-16 15:56:43
+ * @LastEditors: bill Lin_k_Bill@163.com
+ * @LastEditTime: 2022-12-16 16:09:39
+ * @FilePath: /webpack-demo01/src/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+class Test {
+  constructor() {
+    document.write('hello world')
+  }
+}
+
+new Test()
