09bcdf0b4d6adcf1a4ccfa807c616e8e88a21afd
bill <Lin_k_Bill@163.com>
2022-12-17 20:08:23 +0800

    edit：打包后压缩css资源

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: bill Lin_k_Bill@163.com
  * @Date: 2022-12-16 15:57:15
  * @LastEditors: bill Lin_k_Bill@163.com
- * @LastEditTime: 2022-12-16 23:20:14
+ * @LastEditTime: 2022-12-17 20:04:20
  * @FilePath: /webpack-note/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -16,3 +16,4 @@
 - css修改变量命名冲突部分配置有改变，看[官方文档](https://www.npmjs.com/package/css-loader)按需配置
 - 配置postcss-loader还需要下载postcss-import和postcss-url两个依赖
 - 配置打包后抽离css文件，使用了mini-css-extract-plugin就不用使用style-loader插件，两个有冲突，把style-loader移除
+- 打包后压缩css文件的插件optimize-css-assets-webpack-plugin需要webpack4.x，当前项目5.x版本太高安装不了，通过[插件文档](https://www.npmjs.com/package/optimize-css-assets-webpack-plugin)可以看到它推荐我们安装另一个插件``css-minimizer-webpack-plugin``
--- build/webpack.base.js
+++ build/webpack.base.js
@@ -2,7 +2,7 @@
  * @Author: bill Lin_k_Bill@163.com
  * @Date: 2022-12-16 15:56:31
  * @LastEditors: bill Lin_k_Bill@163.com
- * @LastEditTime: 2022-12-16 23:19:37
+ * @LastEditTime: 2022-12-17 20:05:55
  * @FilePath: /webpack-demo01/build/webpack.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const rootDir = process.cwd()
 const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 
 const {
   CleanWebpackPlugin
@@ -46,6 +47,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
     }),
+    new CssMinimizerWebpackPlugin()
   ],
   module: {
     rules: [{
